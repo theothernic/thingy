8abc73bede2f9bebe48027b957e941bd0fae17ca
Nic <theothernic@outlook.com>
2020-04-09 23:01:12 -0400

    working on creating posts (model and migration)

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+    namespace App\Models;
+
+    use App\User;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+
+    class Post extends Model
+    {
+        use SoftDeletes;
+
+        protected $table = 'posts';
+        protected $primaryKey = 'id';
+        protected $fillable = [
+            'user_id',
+            'title',
+            'body',
+        ];
+
+        public function author()
+        {
+            return $this->belongsTo(User::class);
+        }
+    }
