133612ce94364d3a1a2efb20fe56de0371151047
Nic <theothernic@outlook.com>
2020-04-09 22:49:57 -0400

    removing the default laravel welcome route, and adding a front page route instead.

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Http\Controllers;
+
+    class PageController extends Controller
+    {
+        public function front()
+        {
+            return 'Hello, world';
+        }
+    }
