71f7ac88110b7a0830270b9c14beed9a97b011bb
Nic <theothernic@outlook.com>
2020-04-11 01:24:28 -0400

    - adding link back to home when you click on the branding at the top.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
             <meta name="x-ua-compatible" charset="IE=Edge,chrome=1" />
             <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 
-            <title>the feed.</title>
+            <title>the feed. @yield('title')</title>
 
             <link rel="stylesheet" type="text/css" href="{{ @mix('css/app.css') }}" />
             <link rel="stylesheet" type="text/css" href="{{ @mix('css/styles.css') }}" />
@@ -17,7 +17,7 @@
 
             <div class="page container">
                 <header class="masthead">
-                    <h1><span class="thin">the</span><span class="thick">feed</span></h1>
+                    <h1 class="brand"><a href="/"><span class="thin">the</span><span class="thick">feed</span></a></h1>
                 </header>
 
                 <main>
