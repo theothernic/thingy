9ed3e7f6e174e03bde49628a7261865d9916ccbe
Nic <theothernic@outlook.com>
2020-04-20 11:21:40 -0400

    - rethinking and relearning how morph relations work.

--- database/migrations/2020_04_20_145708_create_tags_table.php
+++ database/migrations/2020_04_20_145708_create_tags_table.php
@@ -18,6 +18,7 @@ class CreateTagsTable extends Migration
             $table->string('slug');
             $table->string('name');
             $table->text('description')->nullable();
+            $table->morphs('taggable');
             $table->softDeletes();
             $table->timestamps();
         });
