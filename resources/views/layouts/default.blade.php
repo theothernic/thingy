145e2cee5a610b2e6ad6e7cd0fa25a6b0084ff69
Nic Barr <theothernic@outlook.com>
2020-04-30 14:49:54 -0400

    - display the APP_NAME instead of the feedbag name in the default layout.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
             <meta name="x-ua-compatible" charset="IE=Edge,chrome=1" />
             <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 
-            <title>the feed. @yield('title')</title>
+            <title>{{  @env('APP_NAME') }} :: @yield('title')</title>
             @include('feed::links')
 
             <link rel="stylesheet" type="text/css" href="{{ @mix('css/app.css') }}" />
@@ -18,7 +18,7 @@
 
             <div class="page container">
                 <header class="masthead">
-                    <h1 class="brand"><a href="/"><span class="thin">the</span><span class="thick">feed</span></a></h1>
+                    <h1 class="brand"><a href="/">{{  @env('APP_NAME') }}</a></h1>
                 </header>
 
                 <main>
